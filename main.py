3437b93e3d0cb1649b659275ab1a9260b36ff7af
Zehra Doğan <144070067+dgnzehra@users.noreply.github.com>
2023-09-13 05:03:44 +0300

    Fixing Badge Verification Issues

--- main.py
+++ main.py
@@ -1,112 +1,112 @@
 from PIL import Image, ImageDraw
+import os
+# Creating the badge with restrictions
+def is_valid_badge(image_path, output_directory):
+    issues = []
 
-'''
-The following code below accepts the images only if they are 512x512
-png format (if not png format it gives an error because png format has 4 dimensions RGBA)
-There should be only non transparent pixels in the circle
-The colors in the image should give a happy feeling (here happy feeling criteria is bright colors in the image)
-'''
-def is_happy_badge(image_path):
-    # Load the image
-    img = Image.open(image_path)
+    try:
+        if not os.path.isfile(image_path):
+            issues.append("Image file does not exist")
 
-    issues = [] #an empty list, it is intended to store any issues or problems that are encountered during the analysis of the image
+        with Image.open(image_path) as img:
+            num_channels = len(img.getbands())
+            if img.size != (512, 512):
+                issues.append("Image size does not match the criteria")
 
-    # Check dimensions
-    if img.size != (512, 512):
-        issues.append("Image dimensions are not 512x512.")
+            # Verify transparent and non-transparent pixels along with the circular area
+            alpha = img.getchannel('A')
+            for x in range(512):
+                for y in range(512):
+                    distance_squared = (x - 256) ** 2 + (y - 256) ** 2
+                    if distance_squared <= 256 ** 2 and alpha.getpixel((x, y)) != 0:
+                        pass
+                    elif distance_squared <= 256 ** 2 and alpha.getpixel((x, y)) == 0:
+                        issues.append("circular area has transparent pixels")
+                    elif distance_squared > 256 ** 2 and alpha.getpixel((x, y)) == 0:
+                        pass
+                    elif distance_squared > 256 ** 2 and alpha.getpixel((x, y)) != 0:
+                        issues.append("edges have non-transparent pixels")
 
-    # Verify the circular boundary
-    width, height = img.size
-    mask = Image.new('L', (width, height), 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0, width, height), fill=255)
-    non_transparent = Image.new('RGBA', (width, height))
-    non_transparent.paste(img, (0, 0), mask)
-    non_transparent_pixels = non_transparent.getcolors(width * height)
+        # Analyze happy feeling colors
+        colors = img.getdata()
+        num_happy_colors = 0
+        num_total_pixels = 0
 
-    # Check if non-transparent pixels are within the circle
-    if len(non_transparent_pixels) != 1:
-        issues.append("Non-transparent pixels are not within a circle.")
+        for color in colors:
+            r, g, b, a = color
+            if a != 0:
+                brightness = (r * 299 + g * 587 + b * 114) / 1000
+                num_total_pixels += 1
 
-    # Analyze colors for a "happy" feeling (more bright colors are considered to evoke a "happy" feeling)
-    colors = img.getdata()
-    num_happy_colors = 0
-    num_total_pixels = 0
+                brightness_threshold = 150
 
-    for color in colors:
-        r, g, b, a = color
-        if a != 0:
-            brightness = (r * 299 + g * 587 + b * 114) / 1000
-            num_total_pixels += 1
+                if brightness > brightness_threshold:
+                    num_happy_colors += 1
 
-            brightness_threshold = 150
+        happy_percentage = (num_happy_colors / num_total_pixels) * 100
 
-            if brightness > brightness_threshold:
-                num_happy_colors += 1
+        happy_threshold = 70 #this is a high value, I set it to lower values to get some sample output images
+        if happy_percentage >= happy_threshold:
+            print("happy badge")
+        else:
+            issues.append("not happy feeling badge")
 
-    happy_percentage = (num_happy_colors / num_total_pixels) * 100
+        if not issues:
+            verified_image_path = os.path.join(output_directory, "happy_badge_with_restrictions.png")
+            img.save(verified_image_path)
+            return "Image meets criteria"
 
-    happy_threshold = 70
+    except Exception as e:
+        issues.append(f"An error occurred: {str(e)}")
 
-    return (
-        happy_percentage >= happy_threshold
-        and len(non_transparent_pixels) == 1
-        and img.size == (512, 512)
-    ), issues
-
-def convert_to_happy_badge(input_image_path, output_image_path):
-    img = Image.open(input_image_path)
-
-    img = img.resize((512, 512), Image.ANTIALIAS)
-
-    mask = Image.new('L', (512, 512), 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0, 512, 512), fill=255)
-
-    badge = Image.new('RGBA', (512, 512))
-    badge.paste(img, (0, 0), mask)
-
-    badge.save(output_image_path, 'PNG')
-
-input_path = 'input_image_with_restrictions'
-output_path = 'happy_badge_with_restrictions.png'
-
-result, issues = is_happy_badge(input_path)
-
-if result:
-    convert_to_happy_badge(input_path, output_path)
-    print("Image meets criteria and has been converted to a happy badge.")
-else:
-    print("Image does not meet the criteria for a happy badge. Issues:")
-    for issue in issues:
-        print("- " + issue)
+    if issues:
+        for issue in issues:
+            print(issue)
 
 
-'''
-The following code below does not have any restrictions
-It accepts all formats of images and all sizes
-Also, does not check if the image evokes a happy feeling or not
-'''
-def convert_to_happy_badge(input_image_path, output_image_path):
-    # Load the input image
-    img = Image.open(input_image_path)
+image_path = "C:/Users/dgnze/PycharmProjects/creatingBadge/input_images/purple_cat.png"
+project_directory = "C:/Users/dgnze/PycharmProjects/creatingBadge//output_images"
+verification_result = is_valid_badge(image_path, project_directory)
+if verification_result:
+    print(verification_result)
 
-    # Resize the image to 512x512
-    img = img.resize((512, 512), Image.LANCZOS)
 
-    # Create a circular mask
-    mask = Image.new('L', (512, 512), 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0, 512, 512), fill=255)
+# Creating the badge without restrictions
+def convert_to_happy_badge(input_image_path, project_directory):
+    if not os.path.isfile(input_image_path):
+        return "Input image file does not exist"
 
-    # Apply the mask to the input image
-    badge = Image.new('RGBA', (512, 512))
-    badge.paste(img, (0, 0), mask)
+    try:
+        img = Image.open(input_image_path)
 
-    # Save the resulting badge
-    badge.save(output_image_path, 'PNG')
+        if not img.format:
+            return "Input file is not a valid image"
 
-input_path = 'input_image_without_restrictions'
-output_path = 'happy_badge_without_restrictions.png'
-convert_to_happy_badge(input_path, output_path)
+        img = img.resize((512, 512), Image.LANCZOS)
+
+        mask = Image.new('L', (512, 512), 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((0, 0, 512, 512), fill=255)
+
+        badge = Image.new('RGBA', (512, 512))
+        badge.paste(img, (0, 0), mask)
+
+        output_directory = os.path.join(project_directory, "output_images")
+        os.makedirs(output_directory, exist_ok=True)
+        output_image_path = os.path.join(output_directory, "happy_badge_without_restriction.png")
+
+        badge.save(output_image_path, 'PNG')
+
+        return "Image converted and saved successfully"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+input_path = "C:/Users/dgnze/PycharmProjects/creatingBadge/input_images/purple_cat.png"
+project_directory = "C:/Users/dgnze/PycharmProjects/creatingBadge"
+conversion_result = convert_to_happy_badge(input_path, project_directory)
+print(conversion_result)
+
+
+
+
+
